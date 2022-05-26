38d9a993f133318d6d17ddd69c95d36dae3434ac
Vinicius Ribeiro <vayne99@gmail.com>
2022-05-26 10:15:25 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'white-main':'#FFFFFF',
+        'gray-secondary':'#C0C0C0',
+        'blue-button':'#167BF7',
+        'black-background':'#00030C'
+      }
+    },
+    fontFamily: {
+      'sans':['Inter']
+    }
+  },
+  plugins: [],
+}
